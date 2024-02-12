305affb0d0c82079470d9bc4fcc6e4ca2ea7a04e
rafdewit <rafdewit@hotmail.com>
2024-02-12 18:53:55 +0100

    implementing download functions

--- GradeCalculator/grade-calculator-client/src/modules/classes/components/classes-page/classes-page.component.ts
+++ GradeCalculator/grade-calculator-client/src/modules/classes/components/classes-page/classes-page.component.ts
@@ -8,6 +8,7 @@ import { CreateClassDialogComponent } from './create-class-dialog/create-class-d
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { firstValueFrom } from 'rxjs';
 import { GradeWebClient } from 'src/services/api/grade-web-client.service';
+import { saveAs } from 'file-saver';
 
 @Component({
   selector: 'app-classes-page',
@@ -18,62 +19,70 @@ import { GradeWebClient } from 'src/services/api/grade-web-client.service';
 export class ClassesPageComponent {
   constructor(public gradeStore: GradeStore, private router: Router, private activatedRoute: ActivatedRoute, private matDialog: MatDialog,
     public gradeWebClient: GradeWebClient) {
-    
+
   }
 
   public async updateClass(studentCollection: StudentCollection): Promise<void> {
     const result = await this.openClassDialog();
-    if(result) {
-      await firstValueFrom(this.gradeWebClient.update({id: studentCollection?.id, className: result}));
+    if (result) {
+      await firstValueFrom(this.gradeWebClient.update({ id: studentCollection?.id, className: result }));
     }
   }
 
   public async createClass(): Promise<void> {
     const result = await this.openClassDialog();
-    if(result) {
-      await firstValueFrom(this.gradeWebClient.create({className: result}));
+    if (result) {
+      await firstValueFrom(this.gradeWebClient.create({ className: result }));
     }
   }
 
   public downloadClass(studentCollection: StudentCollection): void {
-    //
+    return saveAs(
+      new Blob([JSON.stringify(studentCollection, null, 2)], { type: 'JSON' }), `${studentCollection.name}_backup.json`
+    );
   }
 
-  public async copyClass(studentCollection: StudentCollection): Promise<void> {
-    const result = await this.openClassDialog();
-    if(result) {
-      await firstValueFrom(this.gradeWebClient.copy({id: studentCollection?.id, className: result}));
-    }
+  public downloadClasses(studentCollections: StudentCollection[]): void {
+    return saveAs(
+      new Blob([JSON.stringify(studentCollections, null, 2)], { type: 'JSON' }), `all_classes_backup.json`
+    );
   }
 
-  public async deleteClass(studentCollection: StudentCollection): Promise<void> {
-    await firstValueFrom(this.gradeWebClient.delete(studentCollection.id));
+  public async copyClass(studentCollection: StudentCollection): Promise < void> {
+  const result = await this.openClassDialog();
+  if(result) {
+    await firstValueFrom(this.gradeWebClient.copy({ id: studentCollection?.id, className: result }));
   }
+}
+
+  public async deleteClass(studentCollection: StudentCollection): Promise < void> {
+  await firstValueFrom(this.gradeWebClient.delete(studentCollection.id));
+}
 
   public classClicked(studentCollection: StudentCollection): void {
-    this.router.navigate([studentCollection.id, "score-overview"], { relativeTo: this.activatedRoute })
-  }
+  this.router.navigate([studentCollection.id, "score-overview"], { relativeTo: this.activatedRoute })
+}
 
   public editClicked(studentCollection: StudentCollection): void {
-    this.router.navigate([studentCollection.id], { relativeTo: this.activatedRoute });
-  }
+  this.router.navigate([studentCollection.id], { relativeTo: this.activatedRoute });
+}
 
-  public async openClassDialog(studentCollection: StudentCollection | null = null): Promise<string | null> {
-    const isEdit = studentCollection !== null;
-    const data: DefaultCrudDialogData<CreateClassDialogData> = {
-      object: {
-        name: studentCollection?.name ?? 'ClassName'
-      },
-      deleteFlag: false,
-      title: isEdit ? 'Update Class' : 'Create Class',
+  public async openClassDialog(studentCollection: StudentCollection | null = null): Promise < string | null > {
+  const isEdit = studentCollection !== null;
+  const data: DefaultCrudDialogData<CreateClassDialogData> = {
+  object: {
+    name: studentCollection?.name ?? 'ClassName'
+  },
+  deleteFlag: false,
+    title: isEdit ? 'Update Class' : 'Create Class',
       cancelFlag: false,
-      isUpdate: isEdit,
+        isUpdate: isEdit,
     }
 
-    const input = new MatDialogConfig<DefaultCrudDialogData<CreateClassDialogData>>();
-    input.data = data;
+const input = new MatDialogConfig<DefaultCrudDialogData<CreateClassDialogData>>();
+input.data = data;
 
-    const dialogRef = this.matDialog.open<CreateClassDialogComponent, DefaultCrudDialogData<CreateClassDialogData>, string>(CreateClassDialogComponent, input);
-    return await firstValueFrom(dialogRef.afterClosed()) ?? null;
+const dialogRef = this.matDialog.open<CreateClassDialogComponent, DefaultCrudDialogData<CreateClassDialogData>, string>(CreateClassDialogComponent, input);
+return await firstValueFrom(dialogRef.afterClosed()) ?? null;
   }
 }
