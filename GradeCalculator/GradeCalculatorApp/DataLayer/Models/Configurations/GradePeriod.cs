f3bb24ce2a8b33638c0210ea2abc7e536b1b010e
rafdewit <rafdewit@hotmail.com>
2024-02-13 13:09:59 +0100

    missing empty constructor

--- GradeCalculator/GradeCalculatorApp/DataLayer/Models/Configurations/GradePeriod.cs
+++ GradeCalculator/GradeCalculatorApp/DataLayer/Models/Configurations/GradePeriod.cs
@@ -2,6 +2,13 @@
 
 public class GradePeriod
 {
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    public GradePeriod()
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    {
+        
+    }
+
     public GradePeriod(string id, string name, List<MultiGradeConfiguration> multiGradeConfigurations,
         List<SingleGradeConfiguration> singleGradeConfigurations)
     {
