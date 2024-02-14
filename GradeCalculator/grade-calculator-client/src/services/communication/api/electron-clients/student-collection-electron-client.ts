b5c497f69da84bf3b769c1a0cdec80120e9ab5b4
rafdewit <rafdewit@hotmail.com>
2024-02-14 14:28:29 +0100

    forgot one

---
+++ GradeCalculator/grade-calculator-client/src/services/communication/api/electron-clients/student-collection-electron-client.ts
@@ -0,0 +1,36 @@
+import { Injectable } from "@angular/core";
+import { Observable, of } from 'rxjs';
+import { StudentCollection } from "../../../dtos/student-collection.model";
+import { UpdateStudentCollectionDto } from "../request/student-collection/update-class-request";
+import { CreateStudentCollectionDto } from "../request/student-collection/create-class-request";
+import { CopyStudentCollectionDto } from "../request/student-collection/copy-class-request";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class StudentCollectionElectronClient {
+    public getAllClasses(): Observable<StudentCollection[]> {
+        return of();
+    }
+
+    public getClass(classId: string): Observable<StudentCollection> {
+        return of();
+    }
+    
+    public updateClass(request: UpdateStudentCollectionDto): Observable<void> {
+        return of();
+    }
+    
+    public copyClass(request: CopyStudentCollectionDto): Observable<void> {
+        return of();
+    }
+    
+    public createClass(request: CreateStudentCollectionDto): Observable<void> {
+        return of();
+    }
+    
+    public deleteClass(id: string): Observable<void> {
+        return of();
+    }
+    
+}
