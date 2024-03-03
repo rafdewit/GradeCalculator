251486a90df6adef03d0856894e5fbe21554b43d
rafdewit <rafdewit@hotmail.com>
2024-03-03 21:54:23 +0100

    moved table filter settings to left side panel

--- GradeCalculator/grade-calculator-client/app/app.ts
+++ GradeCalculator/grade-calculator-client/app/app.ts
@@ -30,7 +30,7 @@ export default class Main {
       },
     });
 
-    this.appWindow.webContents.openDevTools();
+    // this.appWindow.webContents.openDevTools();
     this.appWindow.maximize();
     this.appWindow.loadFile('dist/grade-calculator-client/index.html');
     this.appWindow.on('closed', () => (this.appWindow = null));
