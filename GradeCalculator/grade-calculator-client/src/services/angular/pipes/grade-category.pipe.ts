2be2ea753f8258e3857b2499c12fe1b47c8df72b
Raf <raf.dewit@3folio.finance>
2025-03-18 21:45:59 +0100

    fixing grade representation scale

--- GradeCalculator/grade-calculator-client/src/services/angular/pipes/grade-category.pipe.ts
+++ GradeCalculator/grade-calculator-client/src/services/angular/pipes/grade-category.pipe.ts
@@ -1,27 +1,41 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
+export const scoreParts: ScoreClass[] = getParts();
 @Pipe({ name: 'gradeCategory' })
 export class GradeCategoryPipe implements PipeTransform {
   public transform(n: number | null): string {
-    if(n === null) {
-        return '';
+    if (n === null) {
+      return '';
     }
 
-    const partitionConst = 0.04454545454545;
-    if(n < 0.51) {
-        return '5';
-    } 
-    
-    for(let i = 0; i < 5; i++) {
-        if(n < 0.51 + partitionConst * i * 3 + partitionConst * 1) {
-            return `${4-i}-`
-        } else if(n < 0.5 + partitionConst * i * 3 + partitionConst * 2) {
-            return `${4-i}`
-        } else if(n < 0.5 + partitionConst * i * 3 + partitionConst * 3) {
-            return `${4-i}+`
-        }
+    for (let i = 0; i < scoreParts.length; i++) {
+      if (n < scoreParts[i].border) {
+        return scoreParts[i].score;
+      }
     }
 
     return '';
   }
+}
+
+export function getParts(): ScoreClass[] {
+  const result: ScoreClass[] = [];
+  result.push({ border: 0.51, score: '5' });
+
+  const partitionConst = 0.49 / 3 / 4;
+
+  for (let i = 0; i < 4; i++) {
+    result.push({ border: 0.51 + partitionConst * i * 3 + partitionConst * 1, score: `${4 - i}-` });
+    result.push({ border: 0.51 + partitionConst * i * 3 + partitionConst * 2, score: `${4 - i}` });
+    result.push({ border: 0.51 + partitionConst * i * 3 + partitionConst * 3, score: `${4 - i}+` });
+  }
+
+  result.push({ border: 101, score: '1+' });
+
+  return result;
+}
+
+export class ScoreClass {
+  border: number;
+  score: string;
 }
